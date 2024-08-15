a09f4302800930cc355a561c65240020f1aa3186
Shane Wirkes <swirkes@gmail.com>
2024-08-15 11:37:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XML_Exploration
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filepath = "/Users/swirkes/development/XML_Exploration/xmlsamples/MozartPianoSonata.musicxml";
+            XDocument mozartSonata = XDocument.Load(filepath);
+
+            foreach (XElement element in mozartSonata.Descendants())
+            {
+                //Console.WriteLine(element.Name);
+                if (element.Name == "key")
+                {
+                    var key = element.Value;
+                    Console.WriteLine($"Key is {key}.");
+                }
+            }
+            
+
+        }
+    }
+}
